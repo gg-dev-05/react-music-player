8a2fa4be5db849eb9885ce101435424883fd6233
= <garvitgalgat@gmail.com>
2020-12-04 15:17:23 +0530

    search init

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
+var axios = require("axios").default;
 
 const UserRoutes = require('./routes/User')
 
@@ -14,9 +15,30 @@ app.use(express.json());
 const uri = "mongodb://localhost:27017/music";
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
 
-app.route("/").get((req, res) => {
-    console.log(req)
-    res.send("HELLO WORLD")
+
+
+
+app.route("/search/:q").get((req, res) => {
+    q = req.params.q
+
+    var options = {
+        method: 'GET',
+        url: 'https://deezerdevs-deezer.p.rapidapi.com/search',
+        params: { q: q },
+        headers: {
+            'x-rapidapi-key': '6f2b548fefmsh949d73f0a90fb17p145b15jsn4eeaee9779ea',
+            'x-rapidapi-host': 'deezerdevs-deezer.p.rapidapi.com'
+        }
+    };
+
+    console.log(q)
+
+    axios.request(options).then(function (response) {
+
+        res.send(response.data.data[0].title)
+    }).catch(function (error) {
+        console.error(error);
+    });
 })
 app.use('/user', UserRoutes)
 
@@ -26,4 +48,6 @@ connection.once('open', () => {
 })
 
 app.listen(port, () => console.log(`Server running on port: ${port}`))
+
+
 
